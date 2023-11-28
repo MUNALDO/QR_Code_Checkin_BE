212954ef01ce29a033f4a2cb6e78631a4b2e6614
MunNX <thaiboomnguyen@gmail.com>
2023-11-28 06:04:08 +0100

    update fe scan attendance

--- fe/src/components/ScanAttendance.jsx
+++ fe/src/components/ScanAttendance.jsx
@@ -1,36 +1,13 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import QrScanner from "react-qr-scanner";
 import axios from "axios";
 import { AuthContext } from "../context/AuthContext";
-import { BrowserBarcodeReader } from "@zxing/library";
-import { VideoInputDevice, VideoInputDevices } from "@zxing/library/esm/browser/BrowserCodeReader";
 
 const ScanQR = () => {
   const {
     user: { id: userID },
   } = useContext(AuthContext);
   const [isAttendanceChecked, setAttendanceChecked] = useState(false);
-
-  const [devices, setDevices] = useState < VideoInputDevices > [];
-  const [selectedDevice, setSelectedDevice] =
-    (useState < VideoInputDevice) | (null > null);
-
-  useEffect(() => {
-    const fetchDevices = async () => {
-      try {
-        const videoDevices = await BrowserBarcodeReader.listVideoInputDevices();
-        setDevices(videoDevices);
-        if (videoDevices.length > 0) {
-          // Use the first video device as the default
-          setSelectedDevice(videoDevices[0]);
-        }
-      } catch (error) {
-        console.error("Error listing video devices:", error);
-      }
-    };
-
-    fetchDevices();
-  }, []);
 
   const handleScan = async (data) => {
     if (data && !isAttendanceChecked) {
@@ -66,32 +43,11 @@ const ScanQR = () => {
   return (
     <div className="scan-qr-container">
       <h2>Scan QR Code</h2>
-      {devices.length > 0 && (
-        <>
-          <label htmlFor="cameraSelect">Select Camera:</label>
-          <select
-            id="cameraSelect"
-            onChange={(e) =>
-              setSelectedDevice(
-                devices.find((device) => device.deviceId === e.target.value) ||
-                  null
-              )
-            }
-            value={selectedDevice?.deviceId || ""}
-          >
-            {devices.map((device) => (
-              <option key={device.deviceId} value={device.deviceId}>
-                {device.label || `Camera ${devices.indexOf(device) + 1}`}
-              </option>
-            ))}
-          </select>
-        </>
-      )}
-      <react-zxing-scanner
-        onScan={(data: any) => handleScan(data)}
+      <QrScanner
+        onScan={handleScan}
         onError={handleError}
         style={{ width: "100%" }}
-        deviceId={selectedDevice?.deviceId}
+        facingMode="environment"
       />
     </div>
   );

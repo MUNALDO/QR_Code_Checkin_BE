aebfaa0a28f52870716a87f58bcd09c37f195f07
MunNX <thaiboomnguyen@gmail.com>
2023-11-29 07:06:25 +0100

    add calendar fe v4

--- fe/src/pages/Calendar/Calendar.jsx
+++ fe/src/pages/Calendar/Calendar.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './ScheduleTable.css';
 
 const ScheduleTable = () => {
+  const [selectedYear, setSelectedYear] = useState(new Date());
   const [selectedMonth, setSelectedMonth] = useState(null);
+
+  const handleYearChange = (date) => {
+    setSelectedYear(date);
+    setSelectedMonth(null);
+  };
 
   const handleMonthChange = (date) => {
     setSelectedMonth(date);
@@ -17,27 +24,71 @@ const ScheduleTable = () => {
     return null;
   };
 
+  const getDaysInMonth = (year, month) => {
+    return new Date(year, month + 1, 0).getDate();
+  };
+
+  const renderDaysOfWeek = () => {
+    const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    return daysOfWeek.map((day, index) => (
+      <div key={index} className="day-header">
+        {day}
+      </div>
+    ));
+  };
+
+  const renderCalendarDays = () => {
+    const year = selectedYear.getFullYear();
+    const month = selectedMonth.getMonth();
+
+    const daysInMonth = getDaysInMonth(year, month);
+    const firstDayOfMonth = new Date(year, month, 1);
+    const startingDayOfWeek = firstDayOfMonth.getDay();
+
+    const calendarDays = [];
+
+    for (let i = 0; i < startingDayOfWeek; i++) {
+      calendarDays.push(<div key={`empty-${i}`} className="empty-day"></div>);
+    }
+
+    for (let day = 1; day <= daysInMonth; day++) {
+      calendarDays.push(
+        <div key={day} className="calendar-day">
+          {day}
+        </div>
+      );
+    }
+
+    return calendarDays;
+  };
+
   return (
     <div>
       <h2>Schedule Calendar</h2>
 
-      <Calendar
-        onChange={handleMonthChange}
-        value={selectedMonth}
-        tileContent={renderTileContent}
-        view="year"
-        showNeighboringMonth={false}
-      />
+      <div className="calendar-dropdowns">
+        <Calendar
+          onChange={handleYearChange}
+          value={selectedYear}
+          tileContent={renderTileContent}
+          view="year"
+          showNeighboringMonth={false}
+        />
+        {selectedYear && (
+          <Calendar
+            onChange={handleMonthChange}
+            value={selectedMonth}
+            view="month"
+            showNeighboringMonth={false}
+          />
+        )}
+      </div>
 
       {selectedMonth && (
-        <Calendar
-          value={selectedMonth}
-          view="month"
-          showNeighboringMonth={false}
-          tileContent={({ date, view }) =>
-            view === 'month' ? <div>{date.getDate()}</div> : null
-          }
-        />
+        <div className="calendar-container">
+          <div className="days-of-week">{renderDaysOfWeek()}</div>
+          <div className="calendar-days">{renderCalendarDays()}</div>
+        </div>
       )}
     </div>
   );
